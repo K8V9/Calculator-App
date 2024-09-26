4abb59cac3154f342fca954a9019373b12381d10
K8V9 <vcnkhanh@gmail.com>
2024-09-26 14:40:56 -0400

    Add files via upload

--- index..js
+++ index..js
@@ -1,15 +1,26 @@
 const result = document.getElementById("resultscreen");
+
 function addToResultScreen(input){
     result.value += input;
 }
 function calculate(){
     try {
         result.value = eval(result.value);
+        easterEgg(result.value);
     } catch (error) {
-        result.value = "SYNTAX ERROR"
+        result.value = "SYNTAX ERROR";
+        setTimeout(() => {
+            clearScreen();
+        }, 1000);
     }
 
 }
 function clearScreen(){
     result.value = "";
 }
+function easterEgg(input){
+    if(input == 74713.14){
+        window.alert("You found her! https://ibb.co/xsSkfG1"); 
+    }
+}
+
